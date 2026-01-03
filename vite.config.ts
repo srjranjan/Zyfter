943b2c3d28d82c2e679950d07892071422564ff6
SRJ <Sudhanshuwithyoualll@gmail.com>
2026-01-04 00:12:07 +0530

    fix: remove base path from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Zyfter/',
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
